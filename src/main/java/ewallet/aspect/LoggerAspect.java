d6275b3abda41499d68f5d4c90729dfd57f63656
Silnari <konrad.perl@gmail.com>
2021-05-19 13:16:52 +0200

    Added aspect class to log used methods

---
+++ src/main/java/ewallet/aspect/LoggerAspect.java
@@ -0,0 +1,22 @@
+package ewallet.aspect;
+
+import lombok.extern.java.Log;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Log
+@Aspect
+@Component
+public class LoggerAspect {
+
+    @Before("execution(* ewallet.rest..*.*(..)) || execution(* ewallet.repository..*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info(joinPoint.getTarget().getClass().getSimpleName()
+                + " invokes method " + joinPoint.getSignature().getName()
+                + Arrays.toString(joinPoint.getArgs()));
+    }
+}
